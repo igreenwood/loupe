fe248dee1a2178b72ef2ee464305f55d04dd67fc
igreenwood <i.greenwood.dev@gmail.com>
2020-02-22 21:52:28 +0900

    Add pictures

--- loupe-sample/src/main/java/com/igreenwood/loupesample/util/ImageUrls.kt
+++ loupe-sample/src/main/java/com/igreenwood/loupesample/util/ImageUrls.kt
@@ -6,7 +6,15 @@ object ImageUrls {
         "https://raw.githubusercontent.com/igreenwood/loupe/master/loupe-sample/src/main/res/drawable/photo_by_ishan_at_seefromthesky_on_unsplash.jpg",
         "https://raw.githubusercontent.com/igreenwood/loupe/master/loupe-sample/src/main/res/drawable/photo_by_omid_armin_on_unsplash.jpg",
         "https://raw.githubusercontent.com/igreenwood/loupe/master/loupe-sample/src/main/res/drawable/photo_by_silas_baisch_on_unsplash.jpg",
-        "https://raw.githubusercontent.com/igreenwood/loupe/master/loupe-sample/src/main/res/drawable/photo_by_steven_roe_on_unsplash.jpg"
+        "https://raw.githubusercontent.com/igreenwood/loupe/master/loupe-sample/src/main/res/drawable/photo_by_steven_roe_on_unsplash.jpg",
+        "https://raw.githubusercontent.com/igreenwood/loupe/master/loupe-sample/src/main/res/drawable/photo_by_antonino_visalli_on_unsplash.jpg",
+        "https://raw.githubusercontent.com/igreenwood/loupe/master/loupe-sample/src/main/res/drawable/photo_by_ian_dooley_on_unsplash.jpg",
+        "https://raw.githubusercontent.com/igreenwood/loupe/master/loupe-sample/src/main/res/drawable/photo_by_ibrahim_rifath_on_unsplash.jpg",
+        "https://raw.githubusercontent.com/igreenwood/loupe/master/loupe-sample/src/main/res/drawable/photo_by_isabella_jusková_on_unsplash.jpg",
+        "https://raw.githubusercontent.com/igreenwood/loupe/master/loupe-sample/src/main/res/drawable/photo_by_sergio_souza_on_unsplash.jpg",
+        "https://raw.githubusercontent.com/igreenwood/loupe/master/loupe-sample/src/main/res/drawable/photo_by_shifaaz_shamoon_on_unsplash.jpg",
+        "https://raw.githubusercontent.com/igreenwood/loupe/master/loupe-sample/src/main/res/drawable/photo_by_tom_barrett_on_unsplash.jpg"
+
         )
 
     val multipleImageUrls = listOf(
