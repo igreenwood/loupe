76cfe1087237a13d8a040cfb092ffce9ad2099b2
igreenwood <i.greenwood.dev@gmail.com>
2020-02-22 22:04:50 +0900

    Update sample

--- loupe-sample/src/main/java/com/igreenwood/loupesample/util/ImageUrls.kt
+++ loupe-sample/src/main/java/com/igreenwood/loupesample/util/ImageUrls.kt
@@ -3,18 +3,17 @@ package com.igreenwood.loupesample.util
 object ImageUrls {
 
     val singleImageUrls = listOf(
-        "https://raw.githubusercontent.com/igreenwood/loupe/master/loupe-sample/src/main/res/drawable/photo_by_ishan_at_seefromthesky_on_unsplash.jpg",
-        "https://raw.githubusercontent.com/igreenwood/loupe/master/loupe-sample/src/main/res/drawable/photo_by_omid_armin_on_unsplash.jpg",
-        "https://raw.githubusercontent.com/igreenwood/loupe/master/loupe-sample/src/main/res/drawable/photo_by_silas_baisch_on_unsplash.jpg",
-        "https://raw.githubusercontent.com/igreenwood/loupe/master/loupe-sample/src/main/res/drawable/photo_by_steven_roe_on_unsplash.jpg",
-        "https://raw.githubusercontent.com/igreenwood/loupe/master/loupe-sample/src/main/res/drawable/photo_by_antonino_visalli_on_unsplash.jpg",
-        "https://raw.githubusercontent.com/igreenwood/loupe/master/loupe-sample/src/main/res/drawable/photo_by_ian_dooley_on_unsplash.jpg",
-        "https://raw.githubusercontent.com/igreenwood/loupe/master/loupe-sample/src/main/res/drawable/photo_by_ibrahim_rifath_on_unsplash.jpg",
-        "https://raw.githubusercontent.com/igreenwood/loupe/master/loupe-sample/src/main/res/drawable/photo_by_isabella_juskova_on_unsplash.jpg",
-        "https://raw.githubusercontent.com/igreenwood/loupe/master/loupe-sample/src/main/res/drawable/photo_by_sergio_souza_on_unsplash.jpg",
         "https://raw.githubusercontent.com/igreenwood/loupe/master/loupe-sample/src/main/res/drawable/photo_by_shifaaz_shamoon_on_unsplash.jpg",
-        "https://raw.githubusercontent.com/igreenwood/loupe/master/loupe-sample/src/main/res/drawable/photo_by_tom_barrett_on_unsplash.jpg"
-
+        "https://raw.githubusercontent.com/igreenwood/loupe/master/loupe-sample/src/main/res/drawable/photo_by_ishan_at_seefromthesky_on_unsplash.jpg",
+        "https://raw.githubusercontent.com/igreenwood/loupe/master/loupe-sample/src/main/res/drawable/photo_by_ian_dooley_on_unsplash.jpg",
+        "https://raw.githubusercontent.com/igreenwood/loupe/master/loupe-sample/src/main/res/drawable/photo_by_tom_barrett_on_unsplash.jpg",
+        "https://raw.githubusercontent.com/igreenwood/loupe/master/loupe-sample/src/main/res/drawable/photo_by_antonino_visalli_on_unsplash.jpg",
+        "https://raw.githubusercontent.com/igreenwood/loupe/master/loupe-sample/src/main/res/drawable/photo_by_omid_armin_on_unsplash.jpg",
+        "https://raw.githubusercontent.com/igreenwood/loupe/master/loupe-sample/src/main/res/drawable/photo_by_ibrahim_rifath_on_unsplash.jpg",
+        "https://raw.githubusercontent.com/igreenwood/loupe/master/loupe-sample/src/main/res/drawable/photo_by_steven_roe_on_unsplash.jpg",
+        "https://raw.githubusercontent.com/igreenwood/loupe/master/loupe-sample/src/main/res/drawable/photo_by_silas_baisch_on_unsplash.jpg",
+        "https://raw.githubusercontent.com/igreenwood/loupe/master/loupe-sample/src/main/res/drawable/photo_by_sergio_souza_on_unsplash.jpg",
+        "https://raw.githubusercontent.com/igreenwood/loupe/master/loupe-sample/src/main/res/drawable/photo_by_isabella_juskova_on_unsplash.jpg"
         )
 
     val multipleImageUrls = listOf(
